cb385fa5f8b6b54b1e7d19a1d24378397d60bb8f
earl starks <earlstarks@rocketmail.com>
2022-09-15 09:31:22 -0500

    SwitchStatements

---
+++ SwitchStatements/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine(" What is your favorite school subject?");
+string subject = Console.ReadLine();
+switch (subject)
+{
+	case "math":
+		Console.WriteLine("I love math!");
+		break;
+	case "science":
+	case "physics":
+	case "biology":
+		Console.WriteLine("Science is very awesome subject!");
+		break ;
+	case "history":
+		Console.WriteLine("History is more unseful as you age!");
+        break;
+	case "calculus":
+		Console.WriteLine("too easy amigo!");
+        break;
+	case "english":
+		Console.WriteLine(" English is one of my favs!");
+		break;
+	default:
+		Console.WriteLine(" I don't know that subject!");
+		break;
+}
